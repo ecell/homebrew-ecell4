4eef8131512b9c4cb44480664c1368d11e02a143
Kozo Nishida <knishida@riken.jp>
2015-12-01 20:47:03 +0900

    #3 add centos7 support

--- ecell4.rb
+++ ecell4.rb
@@ -1,5 +1,12 @@
-# prerequisites
+# additional prerequisites for ubuntu
 # apt-get install python-setuptools python-dev
+
+# additional prerequisties for centos7
+# linuxbrew does NOT figure out gcc and g++ in centos. you need to symlink with
+# ln -s `which gcc` `brew --prefix`/bin/gcc-4.8
+# ln -s `which g++` `brew --prefix`/bin/g++-4.8
+# yum install python-setuptools python-devel
+
 class Ecell4 < Formula
   desc "A multi-algorithm, multi-timescale biochemical simulation environment"
   homepage "https://github.com/ecell/ecell4"
@@ -34,6 +41,8 @@ class Ecell4 < Formula
       system "python", *Language::Python.setup_install_args(buildpath/"vendor")
     end
     ENV.prepend_path "PYTHONPATH", buildpath/"vendor/lib/python2.7/site-packages"
+    # centos needs lib64 path
+    ENV.prepend_path "PYTHONPATH", buildpath/"vendor/lib64/python2.7/site-packages"
 
     cd "python" do
       ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
